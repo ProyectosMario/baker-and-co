4cfe7cd8c0878dc52be80ccd9c363671fd832d6a
ProyectosMario <estufa2002@gmail.com>
2026-05-04 21:27:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'baker_co_enterprise'
+    database: process.env.DB_NAME || 'baker_co_enterprise',
+    port: 14939
 });
 
 // Middlewares
