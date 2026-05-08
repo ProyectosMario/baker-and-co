80fc43b7650a61ecc5595d8f46327540f6cc2f60
ProyectosMario <estufa2002@gmail.com>
2026-05-08 19:51:38 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,9 +6,13 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 14939, 
     waitForConnections: true,
-    connectionLimit: 10
+    connectionLimit: 10,
+
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool.promise();
